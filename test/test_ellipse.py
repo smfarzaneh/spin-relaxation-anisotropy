1f785b87a6d9a453db7db639dd53aa357e934f5c
smfarzaneh <smf552@nyu.edu>
2018-04-10 15:47:05 -0400

    added derivative, tangent, and perimeter tests

--- test/test_ellipse.py
+++ test/test_ellipse.py
@@ -13,5 +13,16 @@ class TestEllipse(unittest.TestCase):
         self.assertAlmostEqual(Ellipse.coordinateX(np.pi, 1.0, 2.0), -1.0)
         self.assertAlmostEqual(Ellipse.coordinateY(3.0*np.pi/2.0, 1.0, 2.0), -2.0)
 
+    def testDerivative(self):
+        self.assertAlmostEqual(Ellipse.normDerivative(0.0, 1.0, 2.0), 0.0)
+        self.assertAlmostEqual(Ellipse.normDerivative(np.pi/2.0, 1.0, 2.0), 0.0)
+        self.assertAlmostEqual(Ellipse.normDerivative(np.pi/4.0, 1.0, 1.0), 0.0)
+    
+    def testTangent(self):
+        self.assertAlmostEqual(Ellipse.normTangent(np.pi/4.0, 1.0, 1.0), 1.0)
+
+    def testPerimeter(self):
+        self.assertAlmostEqual(Ellipse.perimeter(1.0, 1.0), 2.0*np.pi)
+        
 if __name__ == '__main__':
     unittest.main()
