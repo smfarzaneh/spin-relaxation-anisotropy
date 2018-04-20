28370146c8eb1b7f26ec5d95ac1157b679ea24a7
smfarzaneh <smf552@nyu.edu>
2018-04-20 14:01:43 -0400

    implemented simpson\s rule

--- src/integral.py
+++ src/integral.py
@@ -2,11 +2,25 @@ import numpy as np
 
 class Integral():
 
-    @classmethod
-    def trapz(cls, x_vals, f_vals):
+    @staticmethod
+    def trapz(x_vals, f_vals):
         num = len(x_vals)
         if num == 1:
             raise ValueError('invalid input array')
         h = (x_vals[num - 1] - x_vals[0])/(num - 1.0) 
         i = h*(np.sum(f_vals) - 0.5*f_vals[0] - 0.5*f_vals[num - 1])
+        return i
+
+    @staticmethod
+    def simpson(x_vals, f_vals):
+        num = len(x_vals)
+        if num == 1:
+            raise ValueError('invalid input array')
+        elif num%2 == 0:
+            print('simpson integration warning, number of grid points should be odd.')
+        h = (x_vals[num - 1] - x_vals[0])/(num - 1.0) 
+        f_vals_short = f_vals[1:num-1]
+        odd_vals = f_vals_short[0::2]
+        even_vals = f_vals_short[1::2]
+        i = h/3.0*(f_vals[0] + f_vals[num - 1] + 4.0*np.sum(odd_vals) + 2.0*np.sum(even_vals))
         return i
--- test/test_integral.py
+++ test/test_integral.py
@@ -4,13 +4,21 @@ import numpy as np
 
 class TestIntegral(unittest.TestCase):
     
-    def testSine(self):
+    def testTrapz(self):
         a = 0.0
         b = np.pi
         num = 101
         x_vals = np.linspace(a, b, num)
         f_vals = np.sin(x_vals)
         self.assertAlmostEqual(Integral.trapz(x_vals, f_vals), 2.0, places=3)
+    
+    def testSimpson(self):
+        a = 0.0
+        b = np.pi
+        num = 101
+        x_vals = np.linspace(a, b, num)
+        f_vals = np.sin(x_vals)
+        self.assertAlmostEqual(Integral.simpson(x_vals, f_vals), 2.0, places=7)
         
 if __name__ == '__main__':
     unittest.main()
