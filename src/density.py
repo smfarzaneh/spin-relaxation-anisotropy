3a74df6408eb82a64ef677395993020e5d7d558b
smfarzaneh <smf552@nyu.edu>
2018-05-17 21:45:49 -0400

    separate spin dependent part

--- src/density.py
+++ src/density.py
@@ -5,8 +5,10 @@ from pauli import *
 class Density(object):
 
     def __init__(self, spin_x, spin_y, spin_z):
+        self.spin = np.zeros((2, 2), dtype=np.complex)
         self.matrix = np.zeros((2, 2), dtype=np.complex)
         self.update(spin_x, spin_y, spin_z)
 
     def update(self, spin_x, spin_y, spin_z):
-        self.matrix = 0.5*Pauli.i() + spin_x*Pauli.x() + spin_y*Pauli.y() + spin_z*Pauli.z()
+        self.spin = spin_x*Pauli.x() + spin_y*Pauli.y() + spin_z*Pauli.z()
+        self.matrix = 0.5*Pauli.i() + self.spin
