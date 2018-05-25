1ddc423a3cb3ffa8e65926da3e928dee72f8d617
smfarzaneh <smf552@nyu.edu>
2018-05-25 13:39:22 -0400

    add defects

--- src/scatterer.py
+++ src/scatterer.py
@@ -3,12 +3,18 @@ import numpy as np
 # Note: All variables are in atomic units (AU). 
 class Scatterer(object):
 
-    def __init__(self, screening_length=0.0, depth=10.0, type='yukawa'):
+    def __init__(self, screening_length=0.0, depth=10.0, potential_type='coulomb'):
         self.l = screening_length
         self.d = depth
+        self.type = potential_type
 
     def potential(self, r):
         return 2.0/r*np.exp(-r/self.l)
     
     def potentialFourier(self, q):
-        return np.divide(2*np.pi*np.exp(-q*self.d), q, out=np.zeros_like(q), where=q!=0)
+        if self.type == 'coulomb':
+            return np.divide(2*np.pi*np.exp(-q*self.d), q, out=np.zeros_like(q), where=q!=0)
+        elif self.type == 'defect':
+            return np.ones_like(q)
+        else: 
+            return np.zeros_like(q)
