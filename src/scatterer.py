f83087225456cfdca38da3715390fb2cdde1f0ba
smfarzaneh <smf552@nyu.edu>
2018-06-09 10:15:39 -0400

    update defect potential

--- src/scatterer.py
+++ src/scatterer.py
@@ -1,20 +1,17 @@
 import numpy as np
 
-# Note: All variables are in atomic units (AU). 
 class Scatterer(object):
 
-    def __init__(self, screening_length=0.0, depth=10.0, potential_type='coulomb'):
-        self.l = screening_length
-        self.d = depth
+    def __init__(self, potential_type='coulomb', coulomb_depth=10.0, defect_radius=5.0, defect_amplitude=1.0):
         self.type = potential_type
-
-    def potential(self, r):
-        return 2.0/r*np.exp(-r/self.l)
+        self.d = coulomb_depth
+        self.sigma = defect_radius
+        self.v_0 = defect_amplitude
     
     def potentialFourier(self, q):
         if self.type == 'coulomb':
             return np.divide(2*np.pi*np.exp(-q*self.d), q, out=np.zeros_like(q), where=q!=0)
         elif self.type == 'defect':
-            return np.ones_like(q)
+            return 2.0*np.pi*self.sigma**2*self.v_0*np.exp(-2.0*np.pi**2*self.sigma**2*q**2)
         else: 
             return np.zeros_like(q)
